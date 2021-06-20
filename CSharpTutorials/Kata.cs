b903ddd2cfa914b43c54d5a42fadac28bd417c16
mertakk <rmertakkaya@gmail.com>
2021-06-21 00:47:56 +0300

    Complete first kata

---
+++ CSharpTutorials/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace CSharpTutorials
+{
+    public class Kata
+    {
+        public Kata()
+        {
+        }
+
+        public int GetSum(int a, int b) => (a != b) ? (a + b) : a;
+
+        public int GetSumBetween(int a, int b)
+        {
+            int sum = 0;
+
+            if (a == b)
+            {
+                return a;
+            }
+            else if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+            else
+            {
+                for (int i = a; i >= b; i--)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+    }
+}
--- CSharpTutorials/Program.cs
+++ CSharpTutorials/Program.cs
@@ -8,6 +8,21 @@ namespace CSharpTutorials
         {
             string message = "SpeciFIX";
             Console.WriteLine(message);
+
+            var kata = new Kata();
+
+            Console.WriteLine(kata.GetSum(3, 5));
+            Console.WriteLine(kata.GetSum(-3, 5));
+            Console.WriteLine(kata.GetSum(5, 5));
+            Console.WriteLine(kata.GetSum(5, -0));
+            Console.WriteLine(kata.GetSum(-5, -5));
+
+            Console.WriteLine(kata.GetSumBetween(3, 5));
+            Console.WriteLine(kata.GetSumBetween(-3, 5));
+            Console.WriteLine(kata.GetSumBetween(5, 5));
+            Console.WriteLine(kata.GetSumBetween(5, -0));
+            Console.WriteLine(kata.GetSumBetween(-5, -5));
+            Console.WriteLine(kata.GetSumBetween(5, 2));
         }
     }
 }
