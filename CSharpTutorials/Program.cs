88fd34c82f2214c83e62db8964f09ce3415d64c4
mertakk <rmertakkaya@gmail.com>
2021-06-20 18:10:53 +0300

    Initial check-in of module CSharpTutorials

---
+++ CSharpTutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTutorials
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
