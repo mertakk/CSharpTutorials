0fc1bdfadd09e97ff1313e264a01f312fa70b3de
mertakk <rmertakkaya@gmail.com>
2021-07-05 02:20:46 +0300

    Initial commit for .NET 5.0

--- CSharpTutorials/Program.cs
+++ CSharpTutorials/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpTutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello SpeciFIX!");
         }
     }
 }
