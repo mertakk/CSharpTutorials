1b8617b8d4a93bba0d58807008efaccf766f796f
mertakk <rmertakkaya@gmail.com>
2021-07-05 02:26:08 +0300

    Copy for .NET 5.0 project

---
+++ CSharpTutorials/Kata.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace CSharpTutorials
+{
+    public class Kata
+    {
+        public Kata()
+        {
+        }
+
+        public int GetSum(int a, int b) => (a != b) ? (a + b) : a;
+
+        public int GetSumBetween(int a, int b)
+        {
+            int sum = 0;
+
+            if (a == b)
+            {
+                return a;
+            }
+            else if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+            else
+            {
+                for (int i = a; i >= b; i--)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+        public string SeriesSum(int input)
+        {
+            if (input == 0)
+            {
+                return "0.00";
+            }
+
+            float sum = 1.00f;
+
+            for (int i = 1; i < input; i++)
+            {
+                sum += (float)1 / (3 * i + 1);
+
+            }
+
+            float roundedSum = (float)Math.Round((float)sum, 2);
+
+            if (roundedSum % 1 == 0)
+            {
+                return roundedSum.ToString() + ".00";
+            }
+            else if ((float)(roundedSum * 10) % 1 == 0)
+            {
+                return roundedSum.ToString() + "0";
+            }
+
+            return roundedSum.ToString();
+        }
+
+        public int FindNextSquare(int input)
+        {
+            if (Math.Sqrt(input) % 1 != 0)
+            {
+                return -1;
+            }
+
+            bool isPerfectFound = false;
+
+            while (!isPerfectFound)
+            {
+                input++;
+                if (Math.Sqrt(input) % 1 == 0)
+                {
+                    isPerfectFound = true;
+                }
+            }
+
+            return input;
+        }
+    }
+}
--- CSharpTutorials/Program.cs
+++ CSharpTutorials/Program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static MyClass;
 
 namespace CSharpTutorials
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello SpeciFIX!");
+            string message = "SpeciFIX";
+            //Console.WriteLine(message);
+
+            var kata = new Kata();
+
+            //Console.WriteLine(kata.GetSum(3, 5));
+            //Console.WriteLine(kata.GetSum(-3, 5));
+            //Console.WriteLine(kata.GetSum(5, 5));
+            //Console.WriteLine(kata.GetSum(5, -0));
+            //Console.WriteLine(kata.GetSum(-5, -5));
+
+            //Console.WriteLine(kata.GetSumBetween(3, 5));
+            //Console.WriteLine(kata.GetSumBetween(-3, 5));
+            //Console.WriteLine(kata.GetSumBetween(5, 5));
+            //Console.WriteLine(kata.GetSumBetween(5, -0));
+            //Console.WriteLine(kata.GetSumBetween(-5, -5));
+            //Console.WriteLine(kata.GetSumBetween(5, 2));
+
+            int implicitlyTypedVariable = 100;
+            var explicitlyTypedVariable = 100;
+
+            var ano = new { name = "Steve" };
+            //Console.WriteLine("Type of ano is {0}", ano.GetType());
+
+            var arr = new[] { 1, 10, 20, 30 };
+            //Console.WriteLine("Type of arr is {0}", arr.GetType());
+
+            IList<string> stringList = new List<string>() {
+                "C# Tutorials",
+                "VB.NET Tutorials",
+                "Learn C++",
+                "MVC Tutorials" ,
+                "Java"
+            };
+
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+
+            var result_2 = stringList.Where(s => s.Contains("Tutorials"));
+
+            ulong soLong = 18446744073709551615ul;
+
+            //Console.WriteLine(kata.SeriesSum(0));
+            //Console.WriteLine(kata.SeriesSum(5));
+            //Console.WriteLine(kata.SeriesSum(3));
+            //Console.WriteLine(kata.SeriesSum(2));
+            //Console.WriteLine(kata.SeriesSum(1));
+            //Console.WriteLine(kata.SeriesSum(79));
+            //Console.WriteLine(kata.SeriesSum(48));
+
+            //Console.WriteLine(kata.FindNextSquare(121));
+            //Console.WriteLine(kata.FindNextSquare(625));
+            //Console.WriteLine(kata.FindNextSquare(114));
+
+            Coordinate point = new Coordinate(10, 20);
+
+            point.x = 20;
+
+            Console.WriteLine(point.x);
+
+            //it is easier to transfer a class object than a struct. So do not use struct when you are passing data across the wire or to other classes.
+            // TODO: Structure alttaki iki kod bloğu incelenecek
+
+            // TODO: Interface'e kadar gelinecek
+
+            int? x = null;
+
+            x ??= 5;
         }
     }
 }
