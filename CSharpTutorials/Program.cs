4d4c7af50d72e3108fc02530ebe3790f729c19f1
mertakk <rmertakkaya@gmail.com>
2021-07-08 02:01:31 +0300

    Complete Value & Reference types

--- CSharpTutorials/Program.cs
+++ CSharpTutorials/Program.cs
@@ -161,6 +161,19 @@ namespace CSharpTutorials
             //Nullable types can only be used with value types.
 
             //You can only use == and != operators with a nullable type. For other comparison use the Nullable static class.
+
+            //STRUCT IS A VALUE TYPE!!!
+
+            //CLASS IS A REFERENCE TYPE!!!
+
+            //When you pass a reference type variable from one method to another, it doesn't create a new copy;
+            //instead, it passes the variable's address. So, If we change the value of a variable in a method,
+            //it will also be reflected in the calling method.
+
+            //String is a reference type, but it is immutable. It means once we assigned a value, it cannot be changed.
+            //If we change a string value, then the compiler creates a new string object in the memory and point a variable to the new memory location.
+            //So, passing a string value to a function will create a new variable in the memory,
+            //and any change in the value in the function will not be reflected in the original value
         }
     }
 }
